2e629d0ee012d188988b731f4b64fcb670e546e9
Satyam0912 <satyamprajapati439@gmail.com>
2022-06-24 07:25:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import MainCartItem from './components/MainCartItem';
+
+function App() {
+  return (
+    <div className="App">
+      <MainCartItem />
+    </div>
+  );
+}
+
+export default App;
